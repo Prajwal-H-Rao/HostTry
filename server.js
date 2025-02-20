4245437256bdfa04012b901e7eb875171df4ef92
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-02-20 11:50:46 +0530

    c

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-const authRoutes = require("./routes/auth");
-const uploadRoute = require("./routes/upload");
+const authRoutes = require("./routes/auth.js");
+const uploadRoute = require("./routes/upload.js");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"));
